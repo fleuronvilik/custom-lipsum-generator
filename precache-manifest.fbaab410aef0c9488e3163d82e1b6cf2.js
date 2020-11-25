f7603aebfebda17e6b6856f990256ab178e4c9d2
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-11-25 23:26:46 +0100

    Updates

---
+++ precache-manifest.fbaab410aef0c9488e3163d82e1b6cf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4a8eb95d975a28a0fb4c4ec54a68fa",
+    "url": "/custom-lipsum-generator/index.html"
+  },
+  {
+    "revision": "8efe896908850eb8f479",
+    "url": "/custom-lipsum-generator/static/css/main.fb86bce3.chunk.css"
+  },
+  {
+    "revision": "b3bf62f9350c64533244",
+    "url": "/custom-lipsum-generator/static/js/2.b1ecee37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/custom-lipsum-generator/static/js/2.b1ecee37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8efe896908850eb8f479",
+    "url": "/custom-lipsum-generator/static/js/main.e3f5f21a.chunk.js"
+  },
+  {
+    "revision": "3a8a68d0662f9e6e2196",
+    "url": "/custom-lipsum-generator/static/js/runtime-main.f16c47c4.js"
+  }
+]);
